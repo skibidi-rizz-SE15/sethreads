7eb933209bd63daf883298170650ebe7c37f9ebc
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-09-09 16:05:00 +0700

    Remove sidebar's top border

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar() {
   const subjects = ["Subject1", "Subject2", "Subject3", "Subject4"];
 
   return (
-    <nav className="flex overflow-hidden z-10 flex-col items-start px-3 pt-10 max-w-full text-base tracking-wide text-white whitespace-nowrap rounded-none border-t border-r border-yellow-600 bg-neutral-800 pb-[608px] w-[303px] max-md:pb-24">
+    <nav className="flex overflow-hidden z-10 flex-col items-start px-3 pt-10 max-w-full text-base tracking-wide text-white whitespace-nowrap rounded-none border-r border-r-yellow-600 bg-neutral-800 pb-[608px] w-[303px] max-md:pb-24">
       <div className="flex overflow-hidden gap-2 px-9 py-1.5 font-bold rounded-lg bg-zinc-600 max-md:px-5">
         <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/48c4f85fabeec6d12399edb61563948596f63b0241a9ce3444c18958f6a5b0c2?placeholderIfAbsent=true&apiKey=55e9f8a1f064422990695f1eab1a40f5" alt="" className="object-contain shrink-0 w-6 aspect-square" />
         <div className="my-auto basis-auto">Home</div>
