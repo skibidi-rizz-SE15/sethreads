a04b0099581eab2547aa4010ebdad1b42a7b5646
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-09-09 16:21:57 +0700

    Remove navbar nesting

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,15 +6,13 @@ import Profile from './Profile/Profile';
 
 function Navbar() {
     return (
-        <div>
-            <header className='flex justify-between items-center overflow-hidden px-20 py-1 rounded-b-xl border-b-2 border-yellow-600 border-solid bg-eerie-black max-md:px-5'>
-                <Logo />
-                <div className='flex items-center'>
-                    <PostBtn />
-                    <Profile />
-                </div>
-            </header>
-        </div>
+        <header className='flex justify-between items-center overflow-hidden px-20 py-1 rounded-b-xl border-b-2 border-yellow-600 border-solid bg-eerie-black max-md:px-5'>
+            <Logo />
+            <div className='flex items-center'>
+                <PostBtn />
+                <Profile />
+            </div>
+        </header>
     );
 }
 
