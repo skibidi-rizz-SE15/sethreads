2c31a96365455fb63caffc4eb24241a66f5641a3
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-09-11 00:19:57 +0700

    Fix inconsistent margin on small screen size

--- src/components/content/threadSection/threadCard/ThreadCard.jsx
+++ src/components/content/threadSection/threadCard/ThreadCard.jsx
@@ -5,10 +5,10 @@ import ThreadBody from '../../../card/threadBody/ThreadBody';
 import ThreadTitle from '../../../card/threadTitle/ThreadTitle';
 
 function ThreadCard({ name, time, title, body, imageHeight, className="" }) {
-  const style = `flex overflow-hidden flex-col self-center pt-6 pb-3.5 mx-auto my-1 rounded-3xl bg-neutral-800 hover:bg-general-highlight max-md:mt-10 ${className}`;
+  const style = `flex overflow-hidden flex-col self-center pt-6 pb-3.5 mx-auto my-1 rounded-3xl bg-neutral-800 hover:bg-general-highlight ${className}`;
   return (
     <article className={style}>
-      <div className="flex flex-col items-start mx-6 max-md:px-5 max-md:max-w-full">
+      <div className="flex flex-col items-start mx-6">
         <Profile name={name} time={time} />
         <ThreadTitle title={title} />
         <ThreadBody body={body} />
