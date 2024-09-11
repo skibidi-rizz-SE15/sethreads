1f44756c85baffa93c7d2ef931a6127552371c8d
WDTX1402 <140149682+WDTX1402@users.noreply.github.com>
2024-09-11 23:18:13 +0700

    fix textBody component

--- src/components/card/textBody/TextBody.jsx
+++ src/components/card/textBody/TextBody.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const TextBody = ({ body, myClass }) => {
-    return <p className="mt-2 text-sm text-stone-500 max-md:max-w-full">{body}</p>
+const TextBody = ({ body, className="" }) => {
+    const style = `mt-2 text-sm text-stone-500 ${className}`;
+    return <p className={style}>{body}</p>
 }
 
 export default TextBody
