2dcb8dacf8e4f7336bc0d83aa549512c19c9302d
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-09-12 23:51:37 +0700

    Add toggle reply button to Comment

--- src/components/threadComponents/commentSection/comment/Comment.jsx
+++ src/components/threadComponents/commentSection/comment/Comment.jsx
@@ -3,13 +3,17 @@ import ReplySection from "./replySection/ReplySection";
 import Profile from "../../../card/profile/Profile";
 import TextBody from "../../../card/textBody/TextBody";
 import ReplyBtn from "../../../button/post/ReplyBtn";
+import ToggleReplyBtn from "../../../button/toggle/ToggleReplyBtn";
 
 const Comment = () => {
     return (
         <div className="mt-2">
             <Profile name={"dummy dummy"}/>
             <TextBody body={'bla bla talk bla comment harewifhweiof hwlikgfhwielgfi wlgfikpwrhgflwhgfiolerh goiewrhgliehrgiehgoiwhr giwheiowhfipwhgfoiw hefiowhfoiwhfooiwegfwkilug hewrilghwr ioghwroighw righwigh wpghwopgfhw roiroghwer ligheroghw roighewrighaerpighewrpigwipghwrpigwpghwgowhghgpohwpoaaaaaaa d'} />
-            <ReplyBtn />
+            <div className="flex gap-1">
+                <ToggleReplyBtn />
+                <ReplyBtn />
+            </div>
             <ReplySection />
         </div>
     )
