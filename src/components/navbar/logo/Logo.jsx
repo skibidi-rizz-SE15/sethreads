838f014e58d9e24b00608aec83dab7f1d8c3a37b
WDTX1402 <140149682+WDTX1402@users.noreply.github.com>
2024-09-17 16:10:27 +0700

    Add hasText prop to Logo component and create sign in page

--- src/components/navbar/logo/Logo.jsx
+++ src/components/navbar/logo/Logo.jsx
@@ -1,9 +1,13 @@
-const Logo = () => {
+const Logo = ({ hasText=true }) => {
     return (
+        hasText
+        ?
         <div className="flex">
             <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/7d8da78db1ff40849a641d3086462423e911d33579caaab958d340cde9701cf2?placeholderIfAbsent=true&apiKey=6c97697ae0354418a18c66f6f8aad447" alt="" className="object-contain shrink-0 self-start aspect-[1.87] w-[58px]" />
             <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/c63b3d1857ca411da165eeff8213e483929388fee54e1d508ae2262a8fa23cd8?placeholderIfAbsent=true&apiKey=6c97697ae0354418a18c66f6f8aad447" alt="Company Logo" className="object-contain shrink-0 max-w-full aspect-[5.15] w-[175px]" />
-        </div>
+        </div> 
+        :
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/7d8da78db1ff40849a641d3086462423e911d33579caaab958d340cde9701cf2?placeholderIfAbsent=true&apiKey=6c97697ae0354418a18c66f6f8aad447" alt="" className="object-contain shrink-0 self-start aspect-[1.87] w-[10rem]" />
     );
 }
 
--- src/pages/LogIn/LoginPage.jsx
+++ src/pages/LogIn/LoginPage.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import SignInBox from "../../components/loginComponents/SignInBox";
+import Logo from "../../components/navbar/logo/Logo";
 
 const LoginPage = () => {
     return (
         <div className="relative w-screen h-screen">
             {/* Left side with logo or content */}
-            <div className="absolute top-0 left-0 w-1/2 h-full bg-black flex justify-center items-center">
-                {/* Placeholder for logo or any content */}
-                <h1 className="text-white text-3xl">Logo / Content</h1>
+            <div className="absolute top-0 left-0 w-1/2 h-full bg-neutral-800 flex justify-center items-center">
+                {/* Enlarged Logo */}
+                <div className="flex justify-center items-center">
+                    <Logo hasText={false} /> {/* Adjust the width and height */}
+                </div>
             </div>
 
             {/* Right side trapezoid */}
-            <div className="absolute top-0 right-0 w-1/2 h-full">
-                <div style={{
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: '#F8F8F8',
-                    clipPath: 'polygon(20% 0%, 100% 0%, 100% 100%, 0% 100%)'
-                }}>
+            <div className="absolute top-0 right-0 w-3/5 h-full">
+                <div style={{width: '100%', height: '100%', backgroundColor: '#F8F8F8', clipPath: 'polygon(20% 0%, 100% 0%, 100% 100%, 0% 100%)'}}>
                 </div>
             </div>
 
