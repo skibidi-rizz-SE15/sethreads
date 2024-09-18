166a315a7654c12153d972e0829c6a937a44be90
WDTX1402 <140149682+WDTX1402@users.noreply.github.com>
2024-09-18 14:57:15 +0700

    Replace dummy sign in form with component

--- src/pages/LogIn/LoginPage.jsx
+++ src/pages/LogIn/LoginPage.jsx
@@ -18,26 +18,8 @@ const LoginPage = () => {
                 <div className="h-full w-10 bg-neutral-600 transform -skew-x-12 "></div>
 
                 {/* white background and form */}
-                <div className="h-full bg-white p-8 flex-grow flex items-center transform -skew-x-12">
-                    <form className="w-full max-w-sm mx-auto">
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                                Username
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                                Password
-                            </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                                Sign In
-                            </button>
-                        </div>
-                    </form>
+                <div className="h-full bg-white p-8 flex-grow flex items-center justify-center transform -skew-x-12">
+                    <SignInBox />
                 </div>
             </div>
         </div>
