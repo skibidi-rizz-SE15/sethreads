af0b12991b17fb21cd6a67817dcc96f8c8d43fc1
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-09-28 16:13:43 +0700

    Implement pasting and dropping images

--- src/components/textEditor/PostThreadEditor.jsx
+++ src/components/textEditor/PostThreadEditor.jsx
@@ -19,7 +19,6 @@ import Superscript from '@tiptap/extension-superscript';
 import Underline from '@tiptap/extension-underline';
 import CharacterCount from '@tiptap/extension-character-count';
 import Dropcursor from '@tiptap/extension-dropcursor';
-import Gapcursor from '@tiptap/extension-gapcursor';
 import FileHandler from '@tiptap-pro/extension-file-handler';
 import Mathematics from '@tiptap-pro/extension-mathematics';
 import DragHandle from '@tiptap-pro/extension-drag-handle-react';
@@ -27,7 +26,7 @@ import { all, createLowlight } from 'lowlight';
 import css from 'highlight.js/lib/languages/css';
 import js from 'highlight.js/lib/languages/javascript';
 import html from 'highlight.js/lib/languages/xml';
-import { common } from 'lowlight';
+// import 'katex/dist/katex.min.css'
 // import styles from `../../styles/tiptapStyles.module.css`;
 
 const lowlight = createLowlight(all)
@@ -63,9 +62,47 @@ const PostThreadEditor = () => {
       Underline,
       CharacterCount,
       Dropcursor,
-      Gapcursor,
       Mathematics,
-      FileHandler,
+      FileHandler.configure({
+        allowedMimeTypes: ['image/png', 'image/jpeg', 'image/gif', 'image/webp'],
+        onDrop: (currentEditor, files, pos) => {
+          files.forEach(file => {
+            const fileReader = new FileReader()
+
+            fileReader.readAsDataURL(file)
+            fileReader.onload = () => {
+              currentEditor.chain().insertContentAt(pos, {
+                type: 'image',
+                attrs: {
+                  src: fileReader.result,
+                },
+              }).focus().run()
+            }
+          })
+        },
+        onPaste: (currentEditor, files, htmlContent) => {
+          files.forEach(file => {
+            if (htmlContent) {
+              // if there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
+              // you could extract the pasted file from this url string and upload it to a server for example
+              console.log(htmlContent) // eslint-disable-line no-console
+              return false
+            }
+
+            const fileReader = new FileReader()
+
+            fileReader.readAsDataURL(file)
+            fileReader.onload = () => {
+              currentEditor.chain().insertContentAt(currentEditor.state.selection.anchor, {
+                type: 'image',
+                attrs: {
+                  src: fileReader.result,
+                },
+              }).focus().run()
+            }
+          })
+        },
+      }),
     ],
     content: '<p>Hello, world!</p>',
   });
