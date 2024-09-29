3767330f1c425bbe437c10ba63a7524e32bd08d7
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-09-29 15:59:37 +0700

    Add title editor

--- src/components/content/CreateThread.jsx
+++ src/components/content/CreateThread.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ThreadBodyEditor from "../textEditor/ThreadBodyEditor";
+import ThreadTitleEditor from "../textEditor/ThreadTitleEditor";
 
 const CreateThread = () => {
     return (
         <main className="flex flex-col overflow-y-auto px-9 pt-10 mx-auto w-full bg-neutral-800 border max-w-[80%] absolute right-0 bottom-0">
+            <ThreadTitleEditor />
             <ThreadBodyEditor />
         </main>
     )
