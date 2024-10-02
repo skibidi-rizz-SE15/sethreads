865f9b4f1199025e9946b06c00cd211f02ff09da
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-10-02 22:10:42 +0700

    Remove debugging statements

--- src/components/content/courseDropdown/CourseDropdown.jsx
+++ src/components/content/courseDropdown/CourseDropdown.jsx
@@ -6,7 +6,6 @@ const CourseDropdown = ({ registeredCourses, setSelectedCourseId }) => {
     return (
         <div className="flex w-[20rem] text-left">
             <select
-                onClick={console.log(selectedCourse)}
                 type="button"
                 className="flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
