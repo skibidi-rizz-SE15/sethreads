79b7c0c041ab741eff5b32e2eef7e800a287795b
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-10-02 23:33:32 +0700

    Format styles

--- src/components/administration/ProfileDisplay.jsx
+++ src/components/administration/ProfileDisplay.jsx
@@ -5,7 +5,7 @@ import ProfileCard from './ProfileCard';
 function ProfileDisplay({ studentInfo }) {
   if (studentInfo.length === 0) {
     return (
-      <div className='w-full grid grid-cols-3 mt-14 justify-items-center gap-5'>
+      <div className='self-stretch grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] mt-14 mx-6 justify-items-center gap-5'>
         <h1 className='text-white text-center text-4xl mx-auto'>No students found</h1>
       </div>
     )
