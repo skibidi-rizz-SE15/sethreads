1db681a46f275b899f18f14b60c5324f20da6dc8
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-10-03 23:01:43 +0700

    Fix thread hitbox

--- src/components/content/threadSection/ThreadSection.jsx
+++ src/components/content/threadSection/ThreadSection.jsx
@@ -18,7 +18,7 @@ const ThreadSection = ({ threads, courseId, isHomePage }) => {
   return (
     <section>
       {threads.map((thread, index) => (
-        <Link to={isHomePage ? `thread/${thread.id}` : `/course/${courseId}/thread/${thread.id}`} key={thread.id}>
+        <Link to={isHomePage ? `thread/${thread.id}` : `/course/${courseId}/thread/${thread.id}`} key={thread.id} className='contents'>
           <ThreadCard name={`${thread.author.name} ${thread.author.surname}`} year={thread.author.year} time={thread.create_at} title={thread.title} body={thread.body} comments={thread.comments} className='min-w-96 w-4/5' />
           {index < threads.length - 1 && (<Separator className='mx-auto w-4/5 min-w-96' />)}
         </Link>
