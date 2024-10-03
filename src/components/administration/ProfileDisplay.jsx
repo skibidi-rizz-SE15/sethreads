fa6f4007652b8f4b583dfe3afdc2cf87c71bc347
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-10-03 16:49:51 +0700

    Fix styles

--- src/components/administration/ProfileDisplay.jsx
+++ src/components/administration/ProfileDisplay.jsx
@@ -9,8 +9,8 @@ function ProfileDisplay({ studentInfo }) {
   
   if (studentInfo.length === 0) {
     return (
-      <div className='self-stretch grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] mt-14 mx-6 justify-items-center gap-5'>
-        <h1 className='text-white text-center text-4xl mx-auto'>No students found</h1>
+      <div className='w-full mt-14'>
+        <h1 className='text-white text-center text-xl mx-auto'>No students found</h1>
       </div>
       )
   }
@@ -21,7 +21,7 @@ function ProfileDisplay({ studentInfo }) {
   }
 
   return (
-    <div className='w-full grid grid-cols-3 mt-14 justify-items-center gap-5'>
+    <div className='self-stretch grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] mt-14 mx-6 justify-items-center gap-5'>
       {studentInfo.map((student) => (
           <ProfileCard key={student._id} student={student} onclick={() => handleCardClick(student)} />
       ))}
