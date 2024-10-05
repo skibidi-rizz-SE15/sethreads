9e392b801599ac042e27a5b7fa57d877a2080cbf
WDTX1402 <140149682+WDTX1402@users.noreply.github.com>
2024-10-05 23:51:51 +0700

    Post button wont allow empty post

--- src/components/content/CreateThread.jsx
+++ src/components/content/CreateThread.jsx
@@ -5,16 +5,16 @@ import CourseDropdown from "./courseDropdown/CourseDropdown";
 import PostThreadBtn from "../button/createThread/postThreadBtn";
 
 const CreateThread = ({ registeredCourses, studentId }) => {
-    //selectedCourseId can be a course ID or 'home'
     const [selectedCourseId, setSelectedCourseId] = useState(null);
-    const [title, setTitle] = useState(""); 
-    const [body, setBody] = useState(""); 
-    let isValid = title && body && selectedCourseId;
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const isValid = title && body && selectedCourseId;
 
     return (
         <main className="flex overflow-y-auto w-full">
             <div className="flex flex-col min-h-full h-max gap-8 px-16 py-8 mx-auto w-[60rem] bg-neutral-800">
-                <h1 className="mb-4 text-center color text-gray-50 font-bold text-2xl">Create Thread</h1>
+                <h1 className="mb-4 text-center text-gray-50 font-bold text-2xl">Create Thread</h1>
                 <CourseDropdown registeredCourses={registeredCourses} setSelectedCourseId={setSelectedCourseId} selectedCourseId={selectedCourseId} />
                 <ThreadTitleEditor onChange={setTitle} />
                 <ThreadBodyEditor onChange={setBody} />
