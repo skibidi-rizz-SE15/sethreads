9b6a750751a9b57c3c9c5a66c280988d3c38adae
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-10-13 20:20:20 +0700

    Wrap search bar on smaller screens

--- src/components/administration/Search.jsx
+++ src/components/administration/Search.jsx
@@ -4,8 +4,8 @@ import { FaSearch } from "react-icons/fa";
 
 function Search({ registeredCourses, onSelectYear, onSelectCourse, onSearch }) {
   return (
-    <div className='flex justify-center mt-10 gap-4 w-fit'>
-      <select className='w-[15rem] min-w-[10rem] h-10 text-white text-center bg-steadfast rounded-md focus:outline-none focus:ring-1 focus:ring-software-orange'>
+    <div className='flex flex-wrap justify-center mt-10 gap-4 mx-8 w-fit'>
+      <select className='flex grow w-[15rem] h-10 text-white text-center bg-steadfast rounded-md focus:outline-none focus:ring-1 focus:ring-software-orange'>
           <option value='' onClick={onSelectYear} className='hidden'>Select Year</option>
           <option value='1' onClick={onSelectYear}>Year 1</option>
           <option value='2' onClick={onSelectYear}>Year 2</option>
@@ -13,14 +13,14 @@ function Search({ registeredCourses, onSelectYear, onSelectCourse, onSearch }) {
           <option value='4' onClick={onSelectYear}>Year 4</option>
           <option value='all' onClick={onSelectYear}>All Years</option>
       </select>
-      <select className='w-[30rem] min-w-[10rem] h-10 text-white text-center bg-steadfast rounded-md focus:outline-none focus:ring-1 focus:ring-software-orange'>
+      <select className='flex grow w-[30rem] h-10 text-white text-center bg-steadfast rounded-md focus:outline-none focus:ring-1 focus:ring-software-orange'>
             <option value='' onClick={onSelectCourse} className='hidden'>Select Course</option>
             {registeredCourses.map((course) => (
                 <option key={course.course_id} value={course.course_id} onClick={onSelectCourse}>{course.name}</option>
             ))}
             <option value='all' onClick={onSelectCourse}>All Courses</option>
       </select>
-      <button className='bg-software-orange hover:bg-software-orange-hover text-white font-bold py-2 px-4 rounded' onClick={onSearch}><FaSearch /></button>
+      <button className='flex justify-center items-center grow bg-software-orange hover:bg-software-orange-hover text-white font-bold py-2 px-4 rounded' onClick={onSearch}><FaSearch /></button>
     </div>
   )
 }
