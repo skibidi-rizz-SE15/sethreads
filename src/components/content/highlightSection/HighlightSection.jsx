55ef9fba047a7651596d5422cc35f67b562e69e7
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-10-13 20:58:25 +0700

    Fix highlight section's wrapping

--- src/components/content/highlightSection/HighlightSection.jsx
+++ src/components/content/highlightSection/HighlightSection.jsx
@@ -3,22 +3,16 @@ import HighlightCard from "./highlightCard/HighlightCard";
 import { Link } from "react-router-dom";
 
 const HighlightSection = ({ highlightThreads, courseId, isHomePage }) => {
-    // const highlights = [
-    //   { title: 'Lorem ipsum dolor sit amet, consector dfgldf dgdlgdf f fdk kffdg dfk fdmgmdfgd ff f d d dllkklm', taName: 'TAs Name' },
-    //   { title: 'Lorem ipsum dolor sit amet, consector', taName: 'TAs Name' },
-    //   { title: 'Lorem ipsum dolor sit amet, consector', taName: 'TAs Name' },
-    // ];
-
     return (
         <section>
           <div className="flex gap-4 self-start mb-4 text-2xl font-medium text-white whitespace-nowrap">   
             <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/3117b074e17582b809fc5215b91fa303a269ef8867f5485b6d1dcc4b7fd50eeb?placeholderIfAbsent=true&apiKey=55e9f8a1f064422990695f1eab1a40f5" alt="Highlight Icon" className="object-contain shrink-0 my-auto w-6 aspect-square" />    
             <div className="basis-auto">Highlight</div>   
           </div>    
-          <div className="flex flex-wrap gap-5 mx-6 max-w-full max-md:flex-col">   
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(18rem,1fr))] gap-5 mx-6 max-w-full">   
             {highlightThreads.map((thread) => (
               <Link to={`${isHomePage ? `thread/${thread.id}` : `/course/${courseId}/thread/${thread.id}`}`} style={{display: 'contents' }} key={thread.id}>  
-                <HighlightCard key={thread.id} title={thread.title} taName={`${thread.author.name} ${thread.author.surname}`} year={thread.author.year} comments={thread.comments} className="flex-1 w-full min-w-72 max-w-[33%] max-[300px]:max-w-full" /> 
+                <HighlightCard key={thread.id} title={thread.title} taName={`${thread.author.name} ${thread.author.surname}`} year={thread.author.year} comments={thread.comments} /> 
               </Link>
             ))}   
           </div>    
--- src/components/content/highlightSection/highlightCard/HighlightCard.jsx
+++ src/components/content/highlightSection/highlightCard/HighlightCard.jsx
@@ -5,7 +5,7 @@ import TextTitle from '../../../card/textTitle/TextTitle';
 
 const HighlightCard = ({ title, taName, year, comments, className="" }) => {
   return (
-    <div className={`flex overflow-hidden flex-col gap-2 items-start py-3 px-2.5 text-xs text-white rounded-2xl border border-solid bg-neutral-800 hover:bg-general-highlight border-neutral-700 max-md:ml-0 max-md:w-full ${className}`}>
+    <div className={`flex overflow-hidden flex-col gap-2 items-start py-3 px-2.5 text-xs text-white rounded-2xl border border-solid bg-neutral-800 hover:bg-general-highlight border-neutral-700 ${className}`}>
       <TextTitle title={title} className='line-clamp-3 text-ellipsis mt-0' />
       <div className='flex items-center mt-auto w-full'>
         <Profile name={taName} year={year} className='mr-auto' />
