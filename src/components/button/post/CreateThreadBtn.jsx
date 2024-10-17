7215878aa741a812e1f1dff3a528685483f57453
Copter <121752252+serayutaka@users.noreply.github.com>
2024-10-17 20:05:43 +0700

    Update CreateThreadBtn.jsx

--- src/components/button/post/CreateThreadBtn.jsx
+++ src/components/button/post/CreateThreadBtn.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { IoAdd } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 
 const CreateThreadBtn = () => {
@@ -11,13 +12,8 @@ const CreateThreadBtn = () => {
 
     return (
         <Link to={`/create-thread`} className='contents'>
-            <button className={`transform transition-transform duration-200 ease-in-out ${isClicked ? 'scale-75' : 'scale-100'}`}onClick={handleClick}>
-                <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/5fa42b4965fba9ec659090d50a7cafa271bc0dbc9396b7a24fac04ee2d98049f?placeholderIfAbsent=true&apiKey=6c97697ae0354418a18c66f6f8aad447"
-                    alt=""
-                    className="object-contain shrink-0 my-auto rounded-md aspect-square w-[30px]"
-                />
+            <button className={`transform transition hover:bg-steadfast rounded-lg duration-200 ease-in-out ${isClicked ? 'scale-75' : 'scale-100'}`} onClick={handleClick}>
+                <IoAdd className='text-3xl text-white transition hover:text-software-orange' />
             </button>
         </Link>
     );
