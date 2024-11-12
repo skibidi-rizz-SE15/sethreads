d36e2d3b53ba2e6d339968f97c56abc008b0ca8d
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-11-12 15:58:50 +0700

    Update highlight cards' dynamic sizes

--- src/components/content/highlightSection/HighlightSection.jsx
+++ src/components/content/highlightSection/HighlightSection.jsx
@@ -9,13 +9,19 @@ const HighlightSection = ({ highlightThreads, courseId, isHomePage }) => {
             <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/3117b074e17582b809fc5215b91fa303a269ef8867f5485b6d1dcc4b7fd50eeb?placeholderIfAbsent=true&apiKey=55e9f8a1f064422990695f1eab1a40f5" alt="Highlight Icon" className="object-contain shrink-0 my-auto w-6 aspect-square" />    
             <div className="basis-auto">Highlight</div>   
           </div>    
-          <div className="grid grid-cols-[repeat(auto-fit,minmax(18rem,1fr))] gap-5 mx-6 max-w-full">   
+          <div className="flex overflow-x-auto pb-2 -mb-2 gap-5 mx-6 max-w-full h-auto">   
             {highlightThreads.map((thread) => (
-              <Link to={`${isHomePage ? `thread/${thread.id}` : `/course/${courseId}/thread/${thread.id}`}`} style={{display: 'contents' }} key={thread.id}>  
-                <HighlightCard key={thread.id} title={thread.title} taName={`${thread.author.name} ${thread.author.surname}`} year={thread.author.year} comments={thread.comments} /> 
+              <Link to={`${isHomePage ? `thread/${thread.id}` : `/course/${courseId}/thread/${thread.id}`}`} style={{display: 'contents'}} key={thread.id}>  
+                <HighlightCard
+                 key={thread.id} 
+                 title={thread.title} 
+                 taName={`${thread.author.name} ${thread.author.surname}`} 
+                 year={thread.author.year} comments={thread.comments} 
+                 className="flex flex-grow flex-shrink-0 w-[18rem] max-w-[25rem] min-h-full"
+                /> 
               </Link>
             ))}   
-          </div>    
+          </div> 
         </section>   
       );  
 }
