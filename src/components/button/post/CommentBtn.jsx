a67222d408e14511b60c74c0105dbc6092d334bd
whatdislol <48853511+whatdislol@users.noreply.github.com>
2024-11-14 20:12:27 +0700

    Fix icon resizing

--- src/components/button/post/CommentBtn.jsx
+++ src/components/button/post/CommentBtn.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CommentBtn = ({ number }) => {
     return (
-        <button className="flex gap-1.5 p-2.5 w-fit text-sm font-medium text-center items-center text-white rounded-lg hover:bg-comment-highlight transition duration-200">
-            <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/f13bec1b6188e12080f057de56f7d5d9096a37c2cb5e66272cc3a877ff656a97?placeholderIfAbsent=true&apiKey=55e9f8a1f064422990695f1eab1a40f5" alt="Comment Icon" className="flex w-5" />
-            <div className='flex'>{number}</div>
+        <button className="flex gap-1.5 p-2.5 w-fit text-sm font-medium text-center items-center text-white rounded-lg hover:bg-comment-highlight">
+            <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/f13bec1b6188e12080f057de56f7d5d9096a37c2cb5e66272cc3a877ff656a97?placeholderIfAbsent=true&apiKey=55e9f8a1f064422990695f1eab1a40f5" alt="Comment Icon" className="min-w-5" />
+            <div>{number}</div>
         </button>
     );
 }
